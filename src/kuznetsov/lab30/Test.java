5816406bda79dce4843ea537914e08156c7c5b6e
Bad Hard <badhard5693@gmail.com>
2022-12-22 14:04:50 +0300

    lab30 done

--- src/kuznetsov/lab30/Test.java
+++ src/kuznetsov/lab30/Test.java
@@ -1,43 +1,61 @@
 package kuznetsov.lab30;
 
+import java.util.Arrays;
+
 public class Test {
     public static void main(String[] args) {
         TableOrdersManager tableOrdersManager = new TableOrdersManager();
         InternetOrdersManager internetOrdersManager = new InternetOrdersManager();
 
         Customer customer;
-        String firstName = "Ivan";
-        String lastName = "Ivanov";
-        int age = 20;
+        String firstName = "Andrew";
+        String lastName = "Kuznetsov";
+        int age = 19;
+
         String city = "Moscow";
-        String street = "Lenina";
-        int building = 1;
-        int apartment = 1;
+        String street = "Pushkinskaya";
+        int building = 3;
+        int apartment = 2;
         int zipCode = 123456;
         char letter = 'A';
         Address address = new Address(city, street, building, apartment, zipCode, letter);
+
         customer = new Customer(firstName, lastName, address, age);
+
         Order order = new InternetOrder(customer);
         Order order1 = new TableOrder();
         Order order2 = new TableOrder();
         Order order3 = new InternetOrder(customer);
-        order.add(new Drink("cola", "0.5", 100, DrinkTypeEnum.COLA, 0));
-        order1.add(new Drink("cola", "0.5", 100, DrinkTypeEnum.COLA, 0));
-        order2.add(new Drink("cola", "0.5", 100, DrinkTypeEnum.COLA, 0));
-        order3.add(new Drink("cola", "0.5", 100, DrinkTypeEnum.COLA, 0));
-        tableOrdersManager.add(order1, 1);
-        tableOrdersManager.add(order2, 2);
-        internetOrdersManager.add(order2);
+        Order order4 = new InternetOrder(customer);
+
+        order.add(new Drink("BEER", "1.5", 320, DrinkTypeEnum.BEER, 5));
+        order1.add(new Drink("BRANDY", "0.5", 512, DrinkTypeEnum.BRANDY, 24));
+        order2.add(new Drink("CHAMPAGNE", "1.0", 233, DrinkTypeEnum.CHAMPAGNE, 2));
+        order3.add(new Drink("BEER", "1.2", 290, DrinkTypeEnum.BEER, 5));
+        order4.add(new Drink("COFFEE", "0.3", 109, DrinkTypeEnum.COFFEE, 0));
+
+        tableOrdersManager.add(order1, 5);
+        tableOrdersManager.add(order2, 7);
+        internetOrdersManager.add(order);
         internetOrdersManager.add(order3);
-        System.out.println(tableOrdersManager.itemsQuantity("cola"));
-        System.out.println(internetOrdersManager.itemsQuantity("cola"));
-        System.out.println(tableOrdersManager.ordersQuantity());
-        System.out.println(internetOrdersManager.ordersQuantity());
-        System.out.println(tableOrdersManager.ordersCostSummary());
-        System.out.println(internetOrdersManager.ordersCostSummary());
-        System.out.println(tableOrdersManager.remove(0));
-        System.out.println(internetOrdersManager.remove());
-        System.out.println(tableOrdersManager.ordersQuantity());
-        System.out.println(internetOrdersManager.ordersQuantity());
+        internetOrdersManager.add(order4);
+
+        System.out.println("Amount of BRANDY in table orders: " + tableOrdersManager.itemsQuantity("BRANDY"));
+        System.out.println("Amount of BEER in table orders: " + internetOrdersManager.itemsQuantity("BEER"));
+
+        System.out.println("Amount of tables orders: " + tableOrdersManager.ordersQuantity());
+        System.out.println("Amount of internet orders: " + internetOrdersManager.ordersQuantity());
+
+        System.out.println("Full cost of table orders: " + tableOrdersManager.ordersCostSummary());
+        System.out.println("Full cost of internet orders: " + internetOrdersManager.ordersCostSummary());
+
+
+        System.out.println("Remove the 5th table order: " + tableOrdersManager.remove(5));
+        System.out.println("Amount of tables orders: " + tableOrdersManager.ordersQuantity());
+        System.out.println("Amount of internet orders: " + internetOrdersManager.ordersQuantity());
+
+        System.out.println("Removed the head internet order with items: " + Arrays.toString(internetOrdersManager.remove().getItems()));
+        System.out.println("Amount of tables orders: " + tableOrdersManager.ordersQuantity());
+        System.out.println("Amount of internet orders: " + internetOrdersManager.ordersQuantity());
     }
 }
